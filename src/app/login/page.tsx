aa4c041e720ca40a9cc596d34b631dd123b66d2a
kaflekundan <kundankafle1998@gmail.com>
2023-10-17 00:44:57 +1100

    basic File Structure Created

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,85 @@
-export default function LoginPage(){
-    return(
-        <div className="flex items-center justify-center">
-            <h1>Login</h1>
+// makes the component client side
+"use client";
+
+import Link from "next/link";
+import React from "react";
+
+import { useRouter } from "next/navigation";
+import { axios } from "axios";
+
+export default function SignUp() {
+    // defining User Value on Signup
+    const [user, setUser] = React.useState({
+        email: "",
+        password: "",
+    });
+
+    // Sign Up Function
+    const onLogin = async () => {};
+
+    return (
+        <div className="min-h-screen flex items-center justify-center bg-black text-white">
+            <div className="bg-gray-900 bg-opacity-70 p-8 rounded-lg shadow-md w-96">
+                <h2 className="text-3xl font-semibold mb-6 text-center">
+                    Log In
+                </h2>
+                <form>
+                    <div className="mb-4">
+                        <label htmlFor="email" className="block text-gray-300">
+                            Email
+                        </label>
+                        <input
+                            type="email"
+                            id="email"
+                            name="email"
+                            value={user.email}
+                            onChange={(event) =>
+                                setUser({
+                                    ...user,
+                                    email: event.target.value,
+                                })
+                            }
+                            className="w-full border rounded-md p-3 bg-gray-800 text-gray-300 focus:outline-none focus:border-blue-500"
+                            placeholder="Your email"
+                        />
+                    </div>
+                    <div className="mb-6">
+                        <label
+                            htmlFor="password"
+                            className="block text-gray-300"
+                        >
+                            Password
+                        </label>
+                        <input
+                            type="password"
+                            id="password"
+                            name="password"
+                            value={user.password}
+                            onChange={(event) =>
+                                setUser({
+                                    ...user,
+                                    password: event.target.value,
+                                })
+                            }
+                            className="w-full border rounded-md p-3 bg-gray-800 text-gray-300 focus:outline-none focus:border-blue-500"
+                            placeholder="Your password"
+                        />
+                    </div>
+                    <button
+                        type="submit"
+                        onClick={onLogin}
+                        className="bg-gray-300 text-black rounded-md px-4 py-2 hover:bg-gray-400 transition-colors w-full"
+                    >
+                        Log In
+                    </button>
+                </form>
+                <p className="mt-4 text-gray-300 text-center">
+                    Don&apos;t have an account?{" "}
+                    <Link href="/signup" className="text-blue-500 hover:underline">
+                        Sign Up
+                    </Link>
+                </p>
+            </div>
         </div>
-    )
+    );
 }
---
+++ src/app/profile/[id]/page.tsx
@@ -0,0 +1,9 @@
+export default function UserProfilePage({params}: any){
+    return(
+        <div>   
+            <h1>Profile Profile</h1>
+            <h1> User Profile Page{params.id}</h1>
+            
+        </div>
+    )
+}
